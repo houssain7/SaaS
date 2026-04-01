ed4715172e2ffce8dcaaf8633b7f5538eef037e3
houssain7 <houssainbouazza1@gmail.com>
2026-04-01 23:36:34 +0100

    HSSIN-9 create users module with profile endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common'
+import { UsersService } from './users.service'
 
 @Controller('users')
-export class UsersController {}
-//
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  // Get profile 
+  @Get('me')
+  getProfile(@Query('userId') userId: string) {
+    if (!userId) {
+      return { message: 'userId required' }
+    }
+
+    return this.service.getProfile(userId)
+  }
+
+  // Get organizations 
+  @Get('me/orgs')
+  getMyOrgs(@Query('userId') userId: string) {
+    if (!userId) {
+      return { message: 'userId required' }
+    }
+
+    return this.service.getUserWithOrganizations(userId)
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../../prisma/prisma.service'
 
 @Injectable()
-export class UsersService { }
-//
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  // Get current user profile
+  async getProfile(userId: string) {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+      },
+    })
+  }
+
+  // Get user with organizations
+  async getUserWithOrganizations(userId: string) {
+    return this.prisma.user.findUnique({
+      where: { id: userId },
+      include: {
+        memberships: {
+          include: {
+            organization: true,
+          },
+        },
+      },
+    })
+  }
+}
